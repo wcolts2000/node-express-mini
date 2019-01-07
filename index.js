5d9573d4b578117a8b96328cdb44e011087b944a
wcolts2000 <wcolts2000@gmail.com>
2019-01-07 12:02:18 -0800

    simple server example with node and express

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+const PORT = 5000;
+
+const server = express();
+
+server.get("/", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+server.get("/api/users/:userid", (req, res) => {
+  const id = req.params.userid;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+
+    .catch(err => res.json(err));
+});
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
